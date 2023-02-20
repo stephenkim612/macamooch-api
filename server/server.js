c7da7bc7be8d54a25f067e05ce73c53eb901636a
stephenkim612 <92137370+stephenkim612@users.noreply.github.com>
2023-02-19 17:06:17 -0800

    cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const router = require('./route.js')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 const port = 8080
