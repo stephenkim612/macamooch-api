8e6407b34b7ffa70e3567b4a78d04bbb13d9b4e0
stephenkim612 <92137370+stephenkim612@users.noreply.github.com>
2023-02-26 14:41:30 -0800

    update

--- server/route.js
+++ server/route.js
@@ -7,15 +7,15 @@ router.post('/signup', controller.setHeader, controller.createAccount, controlle
   res.status(200).json({response: res.locals.account})
 })
 
-router.post('/addEvent', controller.addEvent, (req, res) => {
+router.post('/addEvent', controller.setHeader, controller.addEvent, (req, res) => {
   res.status(200).json({status: 'successfully added'})
 })
 
-router.post('/deleteEvent', controller.deleteEvent, (req, res) => {
+router.post('/deleteEvent', controller.setHeader, controller.deleteEvent, (req, res) => {
   res.status(200).json({status: "successfully deleted"})
 })
 
-router.get('/getAllEvents', controller.getAllEvents, (req, res) => {
+router.get('/getAllEvents', controller.setHeader, controller.getAllEvents, (req, res) => {
   res.status(200).json({response: res.locals.events})
 })
 
